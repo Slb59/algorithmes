07161c655ba0935d9cf91c8613739e51247df044
Slb59 <sylvie.bricout2@laposte.net>
2023-03-27 15:19:36 +0200

    Add test_deque

--- test_deque.py
+++ test_deque.py
@@ -1,7 +1,36 @@
 from collections import deque
 
+def green_path(map):
+    green_path_queue = deque([map[0][0]])
+    i=0
+    for i in range(0,len(map),2):
+        green_path_queue.extend(map[i][1:])
+        green_path_queue.extend(map[i+1][:0:-1])
+    for i in range(len(map)-1,0,-1):
+        green_path_queue.append(map[i][0])
+    return green_path_queue
+
+def forward(value,pos,path):
+    try:
+        new_pos = path.index(value,pos+1)
+    except ValueError:
+        new_pos = path.index(value,0)
+    return new_pos
+
 def main():
-    ...
+    map = [[1,2,3,4,5,6,1,2,3],
+           [2,3,4,5,6,1,2,3,4],
+           [3,4,5,6,5,2,3,4,5],
+           [4,5,6,1,4,3,4,5,6],
+           [5,6,1,2,1,4,5,6,1],
+           [6,1,2,3,6,5,6,1,2],
+           [1,2,3,4,5,6,1,2,3],
+           [2,3,4,5,6,1,2,3,4]]
+    green_gardian_path = green_path(map)
+    green_gardian_pos=69
+    print(len(green_gardian_path))
+    print(green_gardian_path[71])
+    print(forward(4,green_gardian_pos,green_gardian_path))
 
 if __name__ == "__main__":
     main()
